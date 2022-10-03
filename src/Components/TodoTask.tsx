14f254b317f503fe7932e4bf24559d6fa6434f48
Karim Khairalla <karim.khairalla@sportalliance.com>
2022-10-03 21:47:21 +0200

    Added styled Components

--- src/Components/TodoTask.tsx
+++ src/Components/TodoTask.tsx
@@ -1,26 +1,69 @@
 import React from 'react';
 import { Task } from '../interfaces';
+import styled from 'styled-components';
+
+const colors = {
+  red: 'tomato'
+};
 
 interface TodoTaskProps {
   task: Task;
   onCompleteTask(taskNameToDelete: string): void;
 }
 
+const DeleteCTA = styled.button`
+  flex: 20%;
+  height: 100%;
+  border: none;
+  background-color: ${colors.red};
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  color: white;
+  cursor: pointer;
+`;
+
+const Row = styled.div`
+  width: 500px;
+  height: 50px;
+  display: flex;
+  color: white;
+  margin: 15px;
+`;
+
+const Content = styled.div`
+  flex: 80%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Cell = styled.span`
+  display: grid;
+  place-items: center;
+  border: 1px solid #d3d3d3;
+  width: 100%;
+  height: 100%;
+  font-size: 18px;
+  border-right: none;
+  background-color: gray;
+`;
+
 const TodoTask = ({ task, onCompleteTask }: TodoTaskProps) => {
   return (
-    <div className="task">
-      <div className="content">
-        <span>{task.taskName}</span>
-        <span>{task.priority}</span>
-      </div>
-      <button
+    <Row>
+      <Content>
+        <Cell>{task.taskName}</Cell>
+        <Cell>{task.priority}</Cell>
+      </Content>
+      <DeleteCTA
         onClick={() => {
           onCompleteTask(task.taskName);
         }}
       >
         x
-      </button>
-    </div>
+      </DeleteCTA>
+    </Row>
   );
 };
 
